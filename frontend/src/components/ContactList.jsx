22a58f8deaff674f12b94eab91281cdc2a0fa2c0
Lucas <lucasbertoncello1@gmail.com>
2025-04-09 15:12:01 -0300

    add: contact list header

---
+++ frontend/src/components/ContactList.jsx
@@ -0,0 +1,22 @@
+export default function ContactList() {
+  return (
+    <div className="mt-8">
+      {/* contact list header */}
+      <header className="flex justify-between items-center">
+        <strong className="font-bold text-2xl text-[#222]">3 contatos</strong>
+
+        <a
+          href="/"
+          className="text-main font-bold px-4 py-2 border-2 rounded-md transition-all hover:bg-main hover:text-white"
+        >
+          Add new contact
+        </a>
+      </header>
+      {/* contact list header */}
+
+      {/* contact list */}
+
+      {/* contact lsit */}
+    </div>
+  );
+}
