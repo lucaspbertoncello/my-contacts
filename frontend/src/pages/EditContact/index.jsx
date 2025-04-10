fa2b7e464f6b59635b97d875f184cd9c393bb2b7
Lucas <lucasbertoncello1@gmail.com>
2025-04-10 16:39:28 -0300

    created edit and create page w/ page header and form implemented

---
+++ frontend/src/pages/EditContact/index.jsx
@@ -0,0 +1,11 @@
+import Form from "../../components/Form/Form";
+import PageHeader from "../../components/PageHeader";
+
+export default function EditContact() {
+  return (
+    <>
+      <PageHeader title="Edit contact" />
+      <Form buttonLabel="Save changes" />
+    </>
+  );
+}
---
+++ frontend/src/pages/NewContact/index.jsx
@@ -0,0 +1,11 @@
+import PageHeader from "../../components/PageHeader";
+import Form from "../../components/Form/Form";
+
+export default function NewContact() {
+  return (
+    <>
+      <PageHeader title="New contact" />
+      <Form buttonLabel="Create contact" />
+    </>
+  );
+}
