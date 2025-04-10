ea010d783116f1ef14bb31e155feeeb2c8d0a16a
Lucas <lucasbertoncello1@gmail.com>
2025-04-10 01:30:53 -0300

    feat: browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
 import ContactList from "./components/ContactList";
 import Container from "./components/Container";
 import Header from "./components/Header";
+import Routes from "./Routes";
 
 export default function App() {
   return (
-    <Container>
-      <Header />
-      <ContactList />
-    </Container>
+    <Router>
+      <Container>
+        <Header />
+        <Routes />
+      </Container>
+    </Router>
   );
 }
