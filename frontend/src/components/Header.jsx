fe744d6fcdf287981907c5534299be590a9af07c
Lucas <lucasbertoncello1@gmail.com>
2025-04-10 02:10:22 -0300

    refactor: deleted input search from header and implemented to home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,12 +4,6 @@ export default function Header() {
   return (
     <header className="flex flex-col justify-center items-center mt-20">
       <img src={logo} alt="myContacts" />
-
-      <input
-        className="h-12 px-4 mt-12 w-full bg-white outline-0 rounded-3xl drop-shadow-sm"
-        type="text"
-        placeholder="Search contact..."
-      />
     </header>
   );
 }
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -4,61 +4,71 @@ import trash from "../../assets/images/icons/delete.svg";
 
 export default function Home() {
   return (
-    <div className="mt-8">
-      {/* contact list header */}
-      <header className="flex justify-between items-center mb-4">
-        <strong className="font-bold text-2xl text-font-900">3 contatos</strong>
+    <div>
+      <input
+        className="h-12 px-4 mt-12 w-full bg-white outline-0 rounded-3xl drop-shadow-sm"
+        type="text"
+        placeholder="Search contact..."
+      />
 
-        <a
-          href="/"
-          className="text-main font-bold px-4 py-2 border-2 rounded-md transition-all hover:bg-main hover:text-white"
-        >
-          Add new contact
-        </a>
-      </header>
-      {/* contact list header */}
+      <div className="mt-8">
+        {/* contact list header */}
+        <header className="flex justify-between items-center mb-4">
+          <strong className="font-bold text-2xl text-font-900">
+            3 contatos
+          </strong>
 
-      {/* contact list area */}
-      <div>
-        <header>
-          <button className="flex items-center">
-            <span className="mr-2 text-main font-bold">Nome</span>{" "}
-            <img src={arrow} alt="" />
-          </button>
+          <a
+            href="/"
+            className="text-main font-bold px-4 py-2 border-2 rounded-md transition-all hover:bg-main hover:text-white"
+          >
+            Add new contact
+          </a>
         </header>
+        {/* contact list header */}
 
-        {/* card */}
-        <div className="flex items-center justify-between mt-2 mb-2 bg-white p-4 border border-white rounded-sm drop-shadow-sm">
-          {/* card info */}
-          <div className="flex flex-col">
-            <div className="flex items-center mb-2">
-              <strong className="text-lg text-font-900">
-                Lucas Bertoncello
-              </strong>
-              <small className="bg-main-lighter text-main font-bold px-2 py-1 uppercase rounded-sm ml-3">
-                Instagram
-              </small>
-            </div>
-
-            <span className="text-sm text-font-200">lucas@gmail.com</span>
-            <span className="text-sm text-font-200">(41) 99525-7119</span>
-          </div>
-          {/* card info */}
-
-          {/* actions */}
-          <div className="flex gap-4">
-            <a href="/">
-              <img src={edit} alt="Edit" />
-            </a>
-            <button>
-              <img src={trash} alt="Delete" />
+        {/* contact list area */}
+        <div>
+          <header>
+            <button className="flex items-center">
+              <span className="mr-2 text-main font-bold">Nome</span>{" "}
+              <img src={arrow} alt="" />
             </button>
+          </header>
+
+          {/* card */}
+          <div className="flex items-center justify-between mt-2 mb-2 bg-white p-4 border border-white rounded-sm drop-shadow-sm">
+            {/* card info */}
+            <div className="flex flex-col">
+              <div className="flex items-center mb-2">
+                <strong className="text-lg text-font-900">
+                  Lucas Bertoncello
+                </strong>
+                <small className="bg-main-lighter text-main font-bold px-2 py-1 uppercase rounded-sm ml-3">
+                  Instagram
+                </small>
+              </div>
+
+              <span className="text-sm text-font-200">lucas@gmail.com</span>
+              <span className="text-sm text-font-200">(41) 99525-7119</span>
+            </div>
+            {/* card info */}
+
+            {/* actions */}
+            <div className="flex gap-4">
+              <a href="/">
+                <img src={edit} alt="Edit" />
+              </a>
+              <button>
+                <img src={trash} alt="Delete" />
+              </button>
+            </div>
+            {/* actions */}
           </div>
-          {/* actions */}
+          {/* card */}
         </div>
-        {/* card */}
+        {/* contact list area */}
       </div>
-      {/* contact list area */}
     </div>
   );
 }
