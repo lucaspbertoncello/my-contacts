ccd5ae664fb313a66426641fbea92daab92b6023
Lucas <lucasbertoncello1@gmail.com>
2025-04-11 02:23:10 -0300

    feat: error message and style implemented

--- frontend/src/components/Form/FormGroup.jsx
+++ frontend/src/components/Form/FormGroup.jsx
@@ -1,10 +1,23 @@
 import PropTypes from "prop-types";
 
 // component to display field errors
-export default function FormGroup({ children }) {
-  return <div>{children}</div>;
+export default function FormGroup({ children, error }) {
+  return (
+    <div>
+      {children}
+
+      {error && (
+        <small className="text-danger-main mt-2 text-xs block">{error}</small>
+      )}
+    </div>
+  );
 }
 
 FormGroup.propTypes = {
   children: PropTypes.node.isRequired,
+  error: PropTypes.string,
+};
+
+FormGroup.defaultProps = {
+  error: null,
 };
