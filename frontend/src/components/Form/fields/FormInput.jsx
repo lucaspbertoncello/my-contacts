5712870722a75276da3179569ec2940c81314c7a
Lucas <lucasbertoncello1@gmail.com>
2025-04-11 02:32:57 -0300

    feat: custom styles when has error

--- frontend/src/components/Form/fields/FormInput.jsx
+++ frontend/src/components/Form/fields/FormInput.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
 
-export default function FormInput({ placeholder }) {
+export default function FormInput({ placeholder, error }) {
   return (
     <input
       type="text"
-      className="w-full bg-white outline-0 drop-shadow-sm h-12 rounded-sm px-4 border-2 border-transparent focus:border-main transition-all"
+      className={`w-full bg-white outline-0 drop-shadow-sm h-12 rounded-sm px-4 border-2 transition-all
+        ${
+          error
+            ? "border-danger-main focus:border-danger-main text-danger-main"
+            : "border-transparent focus:border-main text-font-900"
+        }`}
       placeholder={placeholder}
     />
   );
@@ -12,4 +17,5 @@ export default function FormInput({ placeholder }) {
 
 FormInput.propTypes = {
   placeholder: PropTypes.string.isRequired,
+  error: PropTypes.bool,
 };
