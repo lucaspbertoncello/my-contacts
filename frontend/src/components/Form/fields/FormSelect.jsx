1c6be5b422a110606e25a8044f0a43d9c3ba9b2f
Lucas <lucasbertoncello1@gmail.com>
2025-04-15 01:24:54 -0300

    fix: add value and onChange props to FormSelect

--- frontend/src/components/Form/fields/FormSelect.jsx
+++ frontend/src/components/Form/fields/FormSelect.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
 
-export default function FormSelect({ children }) {
+export default function FormSelect({ children, value, onChange }) {
   return (
     <select
       className="w-full bg-white outline-0 drop-shadow-sm h-12 rounded-sm px-4 border-2 border-transparent focus:border-main transition-all"
       placeholder="Name"
+      value={value}
+      onChange={onChange}
     >
       {children}
     </select>
@@ -13,4 +15,6 @@ export default function FormSelect({ children }) {
 
 FormSelect.propTypes = {
   children: PropTypes.node.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
