4efb24f2885a2306121ca5e3742ace466d590a59
Lucas <lucasbertoncello1@gmail.com>
2025-04-16 00:46:00 -0300

    feat: name input validator

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 import FormGroup from "./FormGroup";
@@ -7,29 +8,73 @@ import FormSelect from "./fields/FormSelect";
 import FormButton from "./fields/FormButton";
 
 export default function Form({ buttonLabel }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [category, setCategory] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+
+    if (!e.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { fieldName: "name", message: "Name is required" },
+      ]);
+    } else {
+      setErrors((prevState) =>
+        prevState.filter((error) => error.fieldName !== "name")
+      );
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log({ name, email, phone, category });
+  }
+
+  console.log(errors);
+
   return (
-    <form className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4">
       <FormGroup>
-        <FormInput placeholder="Name" />
+        <FormInput
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+        />
       </FormGroup>
 
       <FormGroup>
-        <FormInput placeholder="E-mail" />
+        <FormInput
+          placeholder="E-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <FormInput placeholder="Phone" />
+        <FormInput
+          placeholder="Phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <FormSelect>
+        <FormSelect
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
           <option>Instagram</option>
           <option>Facebook</option>
           <option>Faculdade</option>
         </FormSelect>
       </FormGroup>
 
-      <FormButton disabled>{buttonLabel}</FormButton>
+      <FormButton>{buttonLabel}</FormButton>
     </form>
   );
 }
