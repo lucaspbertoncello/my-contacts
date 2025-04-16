0f2c4f37741f15ce4dc7eaf083f59d1e5ddd96ec
Lucas <lucasbertoncello1@gmail.com>
2025-04-16 02:20:00 -0300

    style: padding change to improve better form UI on mobile

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -1,3 +1,3 @@
 export default function Container({ children }) {
-  return <div className="w-full max-w-[500px] mx-auto">{children}</div>;
+  return <div className="w-full max-w-[500px] mx-auto p-4">{children}</div>;
 }
