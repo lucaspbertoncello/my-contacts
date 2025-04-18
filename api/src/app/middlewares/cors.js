c89a26e1198e6a283d90d0fbd525f434a2f9d115
Lucas <lucasbertoncello1@gmail.com>
2025-04-18 10:47:35 -0300

    feat: allow all http methods

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,4 +1,5 @@
 module.exports = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   next();
 };
