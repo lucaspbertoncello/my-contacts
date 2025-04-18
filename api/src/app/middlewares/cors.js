26448f48f5f9abc0f7e413bb26f8776a33b76c64
Lucas <lucasbertoncello1@gmail.com>
2025-04-18 14:22:33 -0300

    feat: add max-age header to preflight requests

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Max-Age", "10");
   next();
 };
