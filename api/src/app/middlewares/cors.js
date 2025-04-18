6af75105882ab2ae016717db1e0c8f25d3b6e08e
Lucas <lucasbertoncello1@gmail.com>
2025-04-18 10:12:51 -0300

    refactor: cors and error handler middleware moved to folder

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,6 +1,4 @@
-const cors = (req, res, next) => {
+module.exports = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   next();
 };
-
-module.exports = cors;
--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 require("express-async-errors");
+
 const cors = require("./app/middlewares/cors");
+const errorHandler = require("./app/middlewares/errorHandler");
+
 const routes = require("./routes");
 
 const app = express();
@@ -9,10 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors);
 app.use(routes);
-app.use((error, req, res, next) => {
-  console.log(error);
-  res.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3000, () =>
   console.log("🔥 Server started at http://localhost:3000")
