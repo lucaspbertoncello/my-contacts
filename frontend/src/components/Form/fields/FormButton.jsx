55b5068c4de6374268d39b53de9166a03600f935
Lucas <lucasbertoncello1@gmail.com>
2025-04-28 07:19:30 -0300

    fix: prop onClick to button

--- frontend/src/components/Form/fields/FormButton.jsx
+++ frontend/src/components/Form/fields/FormButton.jsx
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
 
-export default function FormButton({ children, disabled }) {
+export default function FormButton({ children, disabled, onClick }) {
   return (
     <button
       disabled={disabled}
+      onClick={onClick}
       className="
       w-full h-12 font-semibold drop-shadow-sm rounded-sm bg-main text-white transition-all cursor-pointer
       hover:bg-main-light active:bg-main
@@ -16,6 +17,7 @@ export default function FormButton({ children, disabled }) {
 }
 
 FormButton.propTypes = {
-  children: PropTypes.node.isRequired,
   disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  children: PropTypes.node.isRequired,
 };
