2aaf5ab77688117eb33e1bd7daf63b41f94c6eba
Lucas <lucasbertoncello1@gmail.com>
2025-04-30 17:33:59 -0300

    fix: cors request headers allowed

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,5 +1,9 @@
 module.exports = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
   res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Max-Age", "10");
   next();
